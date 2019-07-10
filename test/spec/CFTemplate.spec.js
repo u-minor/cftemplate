d797fc35fb6413a667254520ea23ca991de29761
Minoru Nakata <minoru@sprocket.bz>
2019-07-10 17:51:13 +0900

    add entry and output option

--- test/spec/CFTemplate.spec.js
+++ test/spec/CFTemplate.spec.js
@@ -15,19 +15,19 @@ describe('CFTemplate', () => {
 
   describe('build', () => {
     it('should throw no such file or directory error', () => {
-      return expect(CFTemplate.build({ dir: `${__dirname}/../fixture/dummy` })).to.be.rejectedWith(Error, 'ENOENT: no such file or directory')
+      return expect(CFTemplate.build({ dir: `${__dirname}/../fixture/dummy`, entry: 'index.yml' })).to.be.rejectedWith(Error, 'ENOENT: no such file or directory')
     })
 
     it('should throw missing location error', () => {
-      return expect(CFTemplate.build({ dir: `${__dirname}/../fixture/tpl2` })).to.be.rejectedWith(Error, 'JSON Pointer points to missing location: .resources.yml')
+      return expect(CFTemplate.build({ dir: `${__dirname}/../fixture/tpl2`, entry: 'index.yml' })).to.be.rejectedWith(Error, 'JSON Pointer points to missing location: .resources.yml')
     })
 
     it('should throw already exists error', () => {
-      return expect(CFTemplate.build({ dir: `${__dirname}/../fixture/tpl3` })).to.be.rejectedWith(Error, '.resources.yml already exists. Can not merge files under resources.')
+      return expect(CFTemplate.build({ dir: `${__dirname}/../fixture/tpl3`, entry: 'index.yml' })).to.be.rejectedWith(Error, '.resources.yml already exists. Can not merge files under resources.')
     })
 
     it('should return merged YAML', async () => {
-      const data = await CFTemplate.build({ dir: `${__dirname}/../fixture/tpl1` })
+      const data = await CFTemplate.build({ dir: `${__dirname}/../fixture/tpl1`, entry: 'index.yml' })
 
       expect(data).to.equal(fs.readFileSync(`${__dirname}/../fixture/tpl1_out.yml`, 'utf8').toString())
     })
